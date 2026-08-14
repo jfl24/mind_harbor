5e329895e06afad4f7ed9d232cce71879056d3a0
Amit <a.chandel@pm.me>
2026-08-14 01:45:32 -0400

    Ajout de fonctionalité, modification de schéma.
    - fonctionalité de connexion ajoutée via controller
    
    - modification du schéma DB pour modifier le type de la colonne expiresAt pour Date uniquement

--- server/src/controllers/auth/authentification.ts
+++ server/src/controllers/auth/authentification.ts
@@ -18,20 +18,19 @@ async function authInscription(req:Request, res:Response, next: NextFunction) {
     if(user) { return next(new AppError(409, 'CONFLICT_ERROR', 'Courriel indisponible.')); }
 
     try {
-    const mdpHash = await bcrypt.hash(req.body.passwordHash, 10);
-    const user = await prisma.user.create({
-      data: {
-        ...req.body,
-        passwordHash:mdpHash
-      },
-    });
-    res
-      .status(201)
-      .json({
-        "success":true,
-        "data": { id: user.email, role: user.role, cree: user.createdAt },
-        "message": "Utilisateur crée!"
-         });
+        const mdpHash = await bcrypt.hash(req.body.passwordHash, 10);
+        const user = await prisma.user.create({
+            data: {
+                ...req.body,
+                passwordHash:mdpHash
+            },
+        });
+
+        res.status(201).json({
+            "success":true,
+            "data": { id: user.email, role: user.role, cree: user.createdAt },
+            "message": "Utilisateur crée!"
+            });
   } catch (error) {
     if (error instanceof PrismaClientKnownRequestError) {
         return next(new AppError(500, 'DATABASE_ERROR', 'Erreur lors de l\'inscription.'))
@@ -40,9 +39,69 @@ async function authInscription(req:Request, res:Response, next: NextFunction) {
   }
 }
 
-async function authConnexion(req: Request, res: Response) {
+async function authConnexion(req: Request, res: Response, next: NextFunction) {
+    
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email: req.body.email },
+        });
 
+        if (!user) {
+        next(new AppError(401, 'LOGIN_ERROR', 'La connexion a échouée.'))
+        }
+
+        const passwdCheck = await bcrypt.compare(req.body.passwordHash, user!.passwordHash);
+        
+        if (!passwdCheck) {
+        return next(new AppError(401, 'LOGIN_ERROR', 'La connexion a échouée.'))
+        }
+
+        const tokenAcces = jwt.sign(
+        { sub: user!.id, role: user!.role },
+        process.env.JWT_SECRET!,
+        { expiresIn: "15m" },
+        );
+
+        const tokenRefresh = jwt.sign(
+            { sub: user!.id, role: user!.role },
+            process.env.JWT_REFRESH_SECRET!,
+            { expiresIn: "7d"}
+        )
+
+        const expiration = new Date();
+        expiration.setHours(0,0,0,0)
+        expiration.setDate(expiration.getDate() + 7);
+
+        
+        await prisma.refreshToken.create({
+            data:{
+                userId : user!.id,
+                expiresAt : expiration,
+                token : tokenRefresh
+            }   
+        })
+
+        return res.status(201).json({
+            "succes":true,
+            "data": tokenAcces,
+            "message": "Authentification réussi!."
+        })
+
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === "P2002") {
+            console.log(`Prisma error - code: ${error.code}`);
+            return next(new AppError(500, 'DATABASE_ERROR', 'Erreur lors de la connexion.'))
+        }
+        } else if (error instanceof Prisma.PrismaClientValidationError) {
+        console.log(`Prisma error - validation error`);
+        return next(new AppError(500, 'UNKNOWN_ERROR', 'Erreur lors de la connexion.'))
+        }
+    }
 }
+
+
+
 
 async function authRafraichir(req: Request, res: Response) {
 
