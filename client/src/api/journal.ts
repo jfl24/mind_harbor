8f7b58d204bfa9c4256e493e3b8b81bf4aa9169e
Jean-Francois Pierre <e0429114@cmaisonneuve.qc.ca>
2026-08-16 02:56:36 -0400

    fix: ajustement de journal.ts

--- client/src/api/journal.ts
+++ client/src/api/journal.ts
@@ -3,14 +3,26 @@ import {
   type CreateJournalRequest,
   type JournalEntry,
 } from "../../../shared/types/journal";
+import type { ActivityListResponse, Activity } from "../../../shared/types";
 
-// L'appel axios pour POST un entrée de journal vers le backend
+// L'appel axios pour POST une entrée de journal vers le backend
 export async function postEntreeJournal(
   data: CreateJournalRequest,
 ): Promise<JournalEntry> {
   try {
     const response = await api.post("/journal", data);
     return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// L'appel axios pour GET les activités disponibles dans la BDD
+export async function getActivities(): Promise<Activity[]> {
+  try {
+    const response = await api.get<ActivityListResponse>("/activities");
+
+    return response.data.data;
   } catch (error) {
     throw error;
   }
